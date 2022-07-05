244ad95e6549e052b902a5ab5936e6488f6b6b6d
asfand687 <asfandyar687@gmail.com>
2022-07-06 00:54:27 +0500

    removed instagram from footer and added link for twitter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Skull from '../assets/logo.png'
 import { ReactComponent as Twitter } from '../assets/twitter.svg'
 import { ReactComponent as Boat } from '../assets/boat.svg'
-import { ReactComponent as Instagram } from '../assets/insta.svg'
 
 const Footer = () => {
   return (
@@ -11,10 +10,11 @@ const Footer = () => {
         <img src={Skull} className="w-16 mx-auto" alt="Mystery Skulls" />
         <div className="flex gap-x-4">
           <div className="text-white">
-            <Twitter />
+            <a href="https://twitter.com/_MysterySkulls_">
+              <Twitter />
+            </a>
           </div>
           <Boat />
-          <Instagram />
         </div>
       </div>
     </footer>
