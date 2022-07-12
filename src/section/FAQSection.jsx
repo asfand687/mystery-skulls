c737da1c87f155eeda8ae0e4b7c0f90d21c33c4f
asfand687 <asfandyar687@gmail.com>
2022-07-12 23:58:04 +0500

    updated FAQs

--- src/section/FAQSection.jsx
+++ src/section/FAQSection.jsx
@@ -9,34 +9,25 @@ const FAQSection = () => {
         <div className='flex py-16'>
           <article className="flex-1 text-white space-y-6 border-r border-white">
             <div className="space-y-2 text-xl">
-              <h2 className="font-bold">Question One</h2>
-              <p className="w-[26rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h2 className="font-bold">Mint Date?</h2>
+              <p className="w-[26rem]">Approximately 1 month <br />Supply: TBD</p>
             </div>
 
             <div className="space-y-2 text-xl">
-              <h2 className="font-bold">Question Two</h2>
-              <p className="w-[26rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-
-            <div className="space-y-2 text-xl">
-              <h2 className="font-bold">Question Three</h2>
-              <p className="w-[26rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h2 className="font-bold">What is Mystery Skulls?</h2>
+              <p className="w-[26rem]">Mystery Skulls as a project is not portrayed as just an upcoming NFT Collection. There are a lot of deep meanings hidden under the name Mystery Skulls. Mystery Skulls has been in the thoughts for the past year and a half and finally is brought to life better than ever was and will be.</p>
             </div>
           </article>
           <article className="flex-1 flex flex-col items-end text-white space-y-6">
             <div className="space-y-2 text-xl ">
-              <h2 className="font-bold">Question Four</h2>
-              <p className="w-[26rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h2 className="font-bold">Is the team doxxed?</h2>
+              <p className="w-[26rem]">Yes, all the team members are going to introduce them selfs on each AMA
+                What Blockchain is it on: TBD.</p>
             </div>
 
-            <div className="space-y-2 text-xl ">
-              <h2 className="font-bold">Question Five</h2>
-              <p className="w-[26rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-
-            <div className="space-y-2 text-xl ">
-              <h2 className="font-bold">Question Six</h2>
-              <p className="w-[26rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div className="space-y-2 text-xl">
+              <h2 className="font-bold">How to contact the team?</h2>
+              <p className="w-[26rem]">You can contact the team on discord, moderator and founders are always to happy to chat with their community.</p>
             </div>
           </article>
         </div>
